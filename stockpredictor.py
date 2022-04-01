967b086765894259ad3064927b380195c57cb63e
vinc95 <102709002+vinc95@users.noreply.github.com>
2022-04-01 14:36:52 +0800

    Update stockpredictor.py

--- stockpredictor.py
+++ stockpredictor.py
@@ -4,7 +4,8 @@ from datetime import date
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plotly
-from plotly import graph_objs as graph_objs
+from plotly import graph_objs
+import graph_objs as graph_objs
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
