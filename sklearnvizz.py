62ae1beed9c5d4f64c9dae3bddb50cba8069073d
vinc95 <102709002+vinc95@users.noreply.github.com>
2022-04-04 21:06:46 +0800

    Update sklearnvizz.py

--- sklearnvizz.py
+++ sklearnvizz.py
@@ -26,7 +26,7 @@ This is a web app demo using python libraries such as Streamlit, Sklearn etc
 
 st.sidebar.write ("For more info, please contact:")
 
-st.sidebar.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+st.sidebar.write("<a href='https://www.linkedin.com/in/jiajunlok/'>Lok Jia Jun </a>", unsafe_allow_html=True)
 
 
 choice = st.sidebar.radio(
